8f66dc4a3bbacc133c1c1acad915cd0fe23bf25c
Flavio Barbosa <flaviombarbosa@gmail.com>
2023-09-11 10:27:15 -0300

    feat: find artist by name

--- src/main/java/com/github/flaviobarbosa/musicspringgraphql/controller/ArtistController.java
+++ src/main/java/com/github/flaviobarbosa/musicspringgraphql/controller/ArtistController.java
@@ -2,6 +2,7 @@ package com.github.flaviobarbosa.musicspringgraphql.controller;
 
 import com.github.flaviobarbosa.musicspringgraphql.model.Artist;
 import com.github.flaviobarbosa.musicspringgraphql.service.ArtistService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -16,6 +17,11 @@ public class ArtistController {
   @QueryMapping
   public Artist artistById(@Argument int id) {
     return artistService.findById(id);
+  }
+
+  @QueryMapping("artistByName")
+  public List<Artist> findArtistByName(@Argument String name) {
+    return artistService.findByName(name);
   }
 
 }
--- src/main/java/com/github/flaviobarbosa/musicspringgraphql/service/ArtistService.java
+++ src/main/java/com/github/flaviobarbosa/musicspringgraphql/service/ArtistService.java
@@ -3,6 +3,7 @@ package com.github.flaviobarbosa.musicspringgraphql.service;
 import java.util.Arrays;
 import java.util.List;
 import com.github.flaviobarbosa.musicspringgraphql.model.Artist;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,4 +22,9 @@ public class ArtistService {
         .orElse(null);
   }
 
+  public List<Artist> findByName(String name) {
+    return artists.stream()
+        .filter(artist -> artist.name().toLowerCase().contains(name.toLowerCase()))
+        .collect(Collectors.toList());
+  }
 }

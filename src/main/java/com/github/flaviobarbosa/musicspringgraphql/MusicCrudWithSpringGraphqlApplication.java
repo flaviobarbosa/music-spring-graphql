86aee9d9e3ae0343d07dbc8eb19b61c1ea6a45a8
Flavio Barbosa <flaviombarbosa@gmail.com>
2023-09-11 09:17:47 -0300

    initial spring configuration

---
+++ src/main/java/com/github/flaviobarbosa/musicspringgraphql/MusicCrudWithSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.flaviobarbosa.musicspringgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCrudWithSpringGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCrudWithSpringGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/flaviobarbosa/musicspringgraphql/MusicCrudWithSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.flaviobarbosa.musicspringgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCrudWithSpringGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
